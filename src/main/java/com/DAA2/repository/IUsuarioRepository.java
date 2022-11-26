3b1561d818ed83f9a214c33559e1c6fd50c6c7c5
Richard_La_Rosa <toastspeedruns@outlook.com.ar>
2022-11-26 17:27:46 -0500

    Obtener el usuario login y sesion

--- src/main/java/com/DAA2/repository/IUsuarioRepository.java
+++ src/main/java/com/DAA2/repository/IUsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.DAA2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.DAA2.model.Usuario;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+    
+	Optional<Usuario> findByEmail(String email); 
 }
--- src/main/java/com/DAA2/service/IUsuarioService.java
+++ src/main/java/com/DAA2/service/IUsuarioService.java
@@ -7,5 +7,6 @@ import com.DAA2.model.Usuario;
 public interface IUsuarioService {
 	Optional<Usuario> findById(Integer id);
 	Usuario save(Usuario usuario);
+	Optional<Usuario> findByEmail(String email);
 
 }
